ae4f95268d92ea114afa0306757c14f3d8045892
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-25 12:37:24 +0000

    Refresh RPE mobile cache for solar storefront update

--- all/sw.js
+++ all/sw.js
@@ -1,5 +1,6 @@
-const CACHE="rpe-shell-v2";
-const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest"];
+const CACHE="rpe-shell-v3";
+const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest",
+  "./solar-category.js?v=20260925-6"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(SHELL.map(url=>cache.add(url).catch(()=>null)))));
