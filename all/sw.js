4d351f11740e675dcfe666306e44dc61e4de9097
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-25 22:00:25 +0000

    Refresh cache for permanent fourth category

--- all/sw.js
+++ all/sw.js
@@ -1,6 +1,6 @@
-const CACHE="rpe-shell-v7";
+const CACHE="rpe-shell-v8";
 const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest",
-  "./solar-category.js?v=20260925-10"];
+  "./solar-category.js?v=20260925-11"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(SHELL.map(url=>cache.add(url).catch(()=>null)))));
