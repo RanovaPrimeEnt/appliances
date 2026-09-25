72a84b33e4d335d0d33f4436a479c16f2e6befa1
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-25 13:42:58 +0000

    Refresh cache for responsive category rework

--- all/sw.js
+++ all/sw.js
@@ -1,6 +1,6 @@
-const CACHE="rpe-shell-v5";
+const CACHE="rpe-shell-v6";
 const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest",
-  "./solar-category.js?v=20260925-8"];
+  "./solar-category.js?v=20260925-9"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(SHELL.map(url=>cache.add(url).catch(()=>null)))));
