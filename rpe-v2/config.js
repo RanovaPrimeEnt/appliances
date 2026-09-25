779cbb532ce59d8fc846ca28e323dbcb2480186c
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-25 00:51:34 +0000

    Connect My RPE to dedicated Supabase backend

--- rpe-v2/config.js
+++ rpe-v2/config.js
@@ -1,4 +1,4 @@
 window.RPE_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://igaerssbzobutlwvjfwt.supabase.co",
+  supabasePublishableKey: "sb_publishable_NMzJFpXOIJMEH3LW50Cs9g_Otc6tlYr"
 };
