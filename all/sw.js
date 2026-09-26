26c4b4c081cccdcc39673fbc5f40969cef829f72
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-26 03:25:07 +0000

    Refresh cache for calm Gen Z theme

--- all/sw.js
+++ all/sw.js
@@ -1,10 +1,11 @@
-const CACHE="rpe-shell-v18";
+const CACHE="rpe-shell-v19";
 const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest",
+  "./rpe-theme.css?v=20260926-1",
   "./solar-category.js?v=20260926-4",
   "./catalogue-lighting.js?v=20260926-4",
   "./lighting-english.js?v=20260926-1",
   "./lighting-integration.js?v=20260926-7",
-  "./friendly-upgrades.js?v=20260926-7"];
+  "./friendly-upgrades.js?v=20260926-8"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(SHELL.map(url=>cache.add(url).catch(()=>null)))));
