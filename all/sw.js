b57f1f860e33e4687b630f48000ddf5096eae501
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-26 03:15:13 +0000

    Refresh cache for cleaned English lighting imagery

--- all/sw.js
+++ all/sw.js
@@ -1,10 +1,9 @@
-const CACHE="rpe-shell-v17";
+const CACHE="rpe-shell-v18";
 const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest",
   "./solar-category.js?v=20260926-4",
-  "./catalogue-lighting.js?v=20260926-3",
+  "./catalogue-lighting.js?v=20260926-4",
   "./lighting-english.js?v=20260926-1",
-  "./lighting-integration.js?v=20260926-6",
-  "./lighting-overlays.js?v=20260926-4",
+  "./lighting-integration.js?v=20260926-7",
   "./friendly-upgrades.js?v=20260926-7"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
