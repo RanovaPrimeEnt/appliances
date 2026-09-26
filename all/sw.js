475a9e63d4b364b5efeb87903297ad771c4c9957
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-26 11:21:12 +0000

    Refresh cache after removing image translation

--- all/sw.js
+++ all/sw.js
@@ -1,10 +1,9 @@
-const CACHE="rpe-shell-v19";
+const CACHE="rpe-shell-v20";
 const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest",
   "./rpe-theme.css?v=20260926-1",
   "./solar-category.js?v=20260926-4",
-  "./catalogue-lighting.js?v=20260926-4",
-  "./lighting-english.js?v=20260926-1",
-  "./lighting-integration.js?v=20260926-7",
+  "./catalogue-lighting.js?v=20260926-5",
+  "./lighting-integration.js?v=20260926-8",
   "./friendly-upgrades.js?v=20260926-8"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
