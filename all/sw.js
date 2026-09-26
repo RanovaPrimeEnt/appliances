62e927ad7cd99efd4dc4b9349159550961496fdd
RanovaPrimeEnt <ranovaprimeenterprise360@gmail.com>
2026-09-26 02:46:56 +0000

    Refresh cache for category carousel

--- all/sw.js
+++ all/sw.js
@@ -1,10 +1,10 @@
-const CACHE="rpe-shell-v18";
+const CACHE="rpe-shell-v13";
 const SHELL=["./","./site.html","./about.html","./contact.html","./shipping-returns.html","./privacy.html","./terms.html","./rpe-mark.svg","./manifest.webmanifest",
   "./solar-category.js?v=20260926-4",
   "./catalogue-lighting.js?v=20260926-3",
   "./lighting-english.js?v=20260926-1",
   "./lighting-integration.js?v=20260926-6",
-  "./friendly-upgrades.js?v=20260926-2"];
+  "./friendly-upgrades.js?v=20260926-3"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(SHELL.map(url=>cache.add(url).catch(()=>null)))));
